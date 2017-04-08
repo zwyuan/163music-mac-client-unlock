218a5a9d2813c3358bbb0832b2788713edd42e29
kamikat <kamikat@banana.moe>
2017-04-08 14:36:37 +0800

    Update installer

--- install.sh
+++ install.sh
@@ -15,8 +15,16 @@ fi
 
 [ ! -f "unlock.dylib" ] && clang -DOUTSIDE_CHINA -framework Foundation -o unlock.dylib -dynamiclib hijack.m || fail "Cannot compile library."
 
+cp unlock.dylib $PREFIX/MacOS/unlock.dylib || fail "Cannot write target directory."
+
+PASSWORD="czh9r3xgGxdaOw9b8qrizM1FWMBQBAztdCDjbYz5r46a97EFQ0uP5c8EL56rqgC38nmlqVBNlHbgGEjToQeRkPcpLQjuQ1y2TNRYW59euPbbLlVQ32saq2j7TdvfZnSe"
+
+unzip -P "$PASSWORD" "$PREFIX/Resources/resources.pack" && \
+  find webfiles/webapp/pub -type f -depth | xargs -n1 sed -i'.deletethis' 's/encrypt_reponse:!0/encrypt_reponse:!1/g' && rm webfiles/webapp/pub/*.deletethis && \
+  zip -rP "$PASSWORD" resources.pack2 webfiles/ && rm -rf webfiles/ && \
+  mv resources.pack2 "$PREFIX/Resources/resources.pack" || fail "Cannot apply resource patch."
+
 mv $PREFIX/MacOS/NeteaseMusic{,Exec}
-cp unlock.dylib $PREFIX/MacOS/unlock.dylib
 
 cat > $PREFIX/MacOS/NeteaseMusic << EOF
 #!/bin/sh
@@ -25,10 +33,3 @@ exec $PREFIX/MacOS/NeteaseMusicExec
 EOF
 
 chmod +x $PREFIX/MacOS/NeteaseMusic
-
-PASSWORD="czh9r3xgGxdaOw9b8qrizM1FWMBQBAztdCDjbYz5r46a97EFQ0uP5c8EL56rqgC38nmlqVBNlHbgGEjToQeRkPcpLQjuQ1y2TNRYW59euPbbLlVQ32saq2j7TdvfZnSe"
-
-unzip -P "$PASSWORD" "$PREFIX/Resources/resources.pack" && \
-  find webfiles/webapp/pub -type f -depth | xargs -n1 sed -i'.deletethis' 's/encrypt_reponse:!0/encrypt_reponse:!1/g' && rm webfiles/webapp/pub/*.deletethis && \
-  zip -rP "$PASSWORD" resources.pack2 webfiles/ && rm -rf webfiles/ && \
-  mv resources.pack2 "$PREFIX/Resources/resources.pack" || fail "Cannot apply resource patch."
